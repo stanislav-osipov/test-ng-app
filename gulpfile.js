2ef7bcad30a5170d88eb5f7d41767aa9dea6cbfd
Stanislav Osipov <Stanislav_Osipov@epam.com>
2015-12-21 11:03:11 +0400

    finished v1

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ var path = {
     libs: [
       'bower_components/angular/angular.js',
 			'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-animate/angular-animate.js',
 			'bower_components/lodash/lodash.js',
     ],
     scss: {
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 
     angular
         .module('todo', [
-        	'ngRoute'
+        	'ngRoute',
+        	'ngAnimate'
         ]);
 })();
